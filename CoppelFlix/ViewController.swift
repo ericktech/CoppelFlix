0297f42d7a1d02a4ba9ca3f4b4f4c70b91b851ed
Erick Leal <erick.tamezll@uanl.edu.mx>
2022-07-05 15:20:57 -0500

    Initial Commit

---
+++ CoppelFlix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoppelFlix
+//
+//  Created by BRM16031 on 05/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CoppelFlixUITests/CoppelFlixUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  CoppelFlixUITestsLaunchTests.swift
+//  CoppelFlixUITests
+//
+//  Created by BRM16031 on 05/07/22.
+//
+
+import XCTest
+
+class CoppelFlixUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
